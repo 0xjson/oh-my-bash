6d186ff0e85f71a8d461691e8d4f0e3a15bb9e49
Jhan Thng <jhanmar.thng@gmail.com>
2026-01-21 15:02:51 +0800

    Add - Ubuntu 24.04 uses fd-find repository.
    alias fdfind to fd

--- aliases/0xjson.aliases.sh
+++ aliases/0xjson.aliases.sh
@@ -2,6 +2,7 @@
 
 alias cd="z"
 alias ls="exa"
+alias fd="fdfind"
 alias :q="exit"
 alias vi="nvim"
 alias bashconfig="vi ~/.bashrc"
@@ -20,13 +21,12 @@ alias msfc="msfconsole.sh"
 alias burpforward="ssh -D 9090 -q -C -N recon -vv"
 alias reversetunnel-recon="ssh -R 8080:localhost:8080 buntu"
 alias burptunnel="socat TCP4-LISTEN:8080,fork,reuseaddr TCP4:172.20.160.1:8080 &"
-alias burpkill="kill $(ps aux | grep -v grep | grep socat | head -n 2  |awk -F' ' {'print $2'})"
+alias burpkill="kill $(ps aux | grep -v grep | grep socat | head -n 2 | awk -F' ' {'print $2'})"
 
-
-crtsh(){
+crtsh() {
   domain="$1"
-  curl -s https://crt.sh/\?q\=\%25.${domain}\&output\=json \
-    | jq -r '.[].name_value' \
-    | sed 's/\\*\\.//g' \
-    | sort -u
+  curl -s https://crt.sh/\?q\=\%25.${domain}\&output\=json |
+    jq -r '.[].name_value' |
+    sed 's/\\*\\.//g' |
+    sort -u
 }
